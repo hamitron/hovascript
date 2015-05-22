3ba24e0ca087d61905c3799e9f736e5a1455d42c
Hamilton Karl <hamilton@nonfatmedia.com>
2015-05-22 11:37:35 -0700

    99 problems

--- hovascripts/99problems.js
+++ hovascripts/99problems.js
@@ -1,4 +1,25 @@
-// please improve this.
 
-var problems = 99;
 
+function populateArrayofProblems() {
+	problems = new Array();
+	problems.name = 'problems'
+	for (i = 1; i < 100; i++) {
+		problems.push(i);
+	};
+	return problems;
+}
+
+function setAdditionalProblem(problem_name) {
+	if (isNaN(problem_name)) {
+		alert("I've got " + String(problems.length) + " " + problems.name + ", but a " + problem_name + " ain't " + String(problems[0]));		}
+		else {
+			problems.push(parseInt(name))
+		};
+}
+
+function hitMe(){
+	populateArrayofProblems();
+	setAdditionalProblem("bitch");
+}
+
+hitMe();
